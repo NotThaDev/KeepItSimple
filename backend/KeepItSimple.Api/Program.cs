9c06d8196740ed2b4823600b81e7bec1db0c9285
Davide Brancato <davidebrancato03@gmail.com>
2026-04-26 16:39:58 +0200

    Expense Tracker API (#2)

--- backend/KeepItSimple.Api/Program.cs
+++ backend/KeepItSimple.Api/Program.cs
@@ -1,7 +1,24 @@
+using KeepItSimple.Api.Helpers;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<KeepItSimpleDbContext>((serviceProvider, options) =>
+{
+    var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+    var connectionString = configuration.GetConnectionString("PostgresConnection");
+    if (string.IsNullOrEmpty(connectionString))
+    {
+        throw new InvalidOperationException("Postgres connection string is not configured.");
+    }
+
+    options.UseNpgsql(connectionString);
+});
+
 
 var app = builder.Build();
 
@@ -12,5 +29,7 @@ if (app.Environment.IsDevelopment())
 
 }
 
+KeepItSimpleContext.InitContext(app);
 app.UseHttpsRedirection();
+app.MapControllers();
 app.Run();
